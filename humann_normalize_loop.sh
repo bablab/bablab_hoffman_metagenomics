6861fa2b88e7d77c819fe18bba36bae52513af89
Fran Querdasi <44685326+fquerdasi@users.noreply.github.com>
2024-04-23 21:48:32 -0700

    minor edits for clarity

--- humann_normalize_loop.sh
+++ humann_normalize_loop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# use this script to run total sum squares normalization (i.e. for maaslin2 input) on
+# use this script to run copies per million normalization (i.e. for maaslin2 input) on
 ## humann output
 
 # apply loop to files (gene families, path abundance, path coverage for each sample) in humann output dir
